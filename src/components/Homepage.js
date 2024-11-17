5248f29d5b680018eb4664549948b4dacdbfa4fc
Spencer Hamilton <shamilton05@berkeley.edu>
2024-11-16 20:54:39 -0800

    Adjust homepage styling

--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -1,23 +1,30 @@
 import React from 'react';
 import './Homepage.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'; // import FontAwesome component
+import { faShoppingBag } from '@fortawesome/free-solid-svg-icons';
+
 
 const Homepage = () => {
   return (
     <>
       {/* Header */}
       <header className="BabylonHeader">
-        <div className="logo">kapital</div>
-        <nav className="nav-links">
+      <nav className="nav-links">
           <a href="#">Collection</a>
           <a href="#">Journal</a>
           <a href="#">Brand</a>
           <a href="#">Stores</a>
         </nav>
+        <div className="logo">kapital</div>
         <div className="account-links">
           <a href="#">Support</a>
           <a href="#">Wishlist</a>
           <a href="#">Account</a>
-          <a href="#">Basket</a>
+          <a href="#" className="shopping-cart-link">
+            <span>Basket</span>
+            <FontAwesomeIcon icon={faShoppingBag} className="shopping-cart-icon" />
+          </a>
+          
         </div>
       </header>
 
